0b2c59fa3f1bf2937dbed061d1390d4562630357
Simon Chretien <simon.chretien62@gmail.com>
2021-02-24 10:29:27 +0100

    Demo end

---
+++ app/models/restaurant.rb
@@ -0,0 +1,7 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+end
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :restaurants do
+  #   collection do
+  #     get :top
+  #   end
+
+  #   member do
+  #     get :chef
+  #   end
+  # end
+
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
+
+  resources :reviews, only: [ :destroy ]
+
+
 end
+
+
+
